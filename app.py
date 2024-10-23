a60bb6c5fd04b6b864e48b30a494231aea726199
Fareed Khan <115370893+fareedqk@users.noreply.github.com>
2024-10-23 20:55:39 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from huggingface_hub import login
 from transformers import pipeline
 
 # Retrieve the Hugging Face token from Colab's secrets
-# HF_TOKEN = userdata.get('HF_TOKEN')
+HF_TOKEN = userdata.get('HF_TOKEN')
 
-# # Authenticate using the retrieved Hugging Face token
-# login(token=HF_TOKEN)
+# Authenticate using the retrieved Hugging Face token
+login(token=HF_TOKEN)
 
 # Set up the text generation pipeline using GPT-2
 generator = pipeline("text-generation", model="gpt2")
