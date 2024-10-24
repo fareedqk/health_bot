b6e1dbf50a7b7b2988972ab82bf5cdd5e0e10fc1
Fareed Khan <115370893+fareedqk@users.noreply.github.com>
2024-10-24 18:31:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-from google.colab import userdata
+# from google.colab import userdata
 from huggingface_hub import login
 from transformers import pipeline
 
 # Retrieve the Hugging Face token from Colab's secrets
-HF_TOKEN = userdata.get('HF_TOKEN')
+# HF_TOKEN = userdata.get('HF_TOKEN')
 
 # Authenticate using the retrieved Hugging Face token
 login(token=HF_TOKEN)
